283813c3a86389d44d7f695448136adafb64ce84
Omega-Xi <akhilantony674@gmail.com>
2025-12-03 08:59:12 +0530

    Added Mobile Responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const menuBtn=document.getElementById("mobile-menu-btn");
+const mobileMenu=document.querySelector(".mobile-nav-links")
+const sections = document.querySelectorAll('section');
+menuBtn.addEventListener('click', function(e) {
+  e.stopPropagation();
+  mobileMenu.style.display = 
+    mobileMenu.style.display === "flex" ? "none" : "flex";
+});
+document.addEventListener('click', (e) => {
+  if (e.target !== menuBtn) {
+    mobileMenu.style.display = "none";
+  }
+});
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
 document.querySelectorAll('.nav-link').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -5,4 +25,7 @@ document.querySelectorAll('.nav-link').forEach(anchor => {
       behavior: 'smooth'
     });
   });
+});
+sections.forEach(section => {
+  observer.observe(section);
 });
